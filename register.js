d33c4f0f541601ef414f502e1695b22a61c17b75
mariaan1 <maria.ths28@gmail.com>
2026-04-02 20:36:06 +0800

    added js register

---
+++ register.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDD3uJlu_rT4DA4jnjyzixRRYc_69r8SL0",
+    authDomain: "stationery-dispenser.firebaseapp.com",
+    projectId: "stationery-dispenser",
+    storageBucket: "stationery-dispenser.firebasestorage.app",
+    messagingSenderId: "57000519693",
+    appId: "1:57000519693:web:748481665644e9c5124d44"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
